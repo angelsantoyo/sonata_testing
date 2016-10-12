0b5027aafe1cec31a41c4431a77e59f2e9eddb0a
Angel Santoyo <angel@gaiadesign.com.mx>
2016-10-12 15:18:27 -0500

    Fix missing configurations for init basic sonata admin config

--- src/WMS/BackendBundle/Controller/DefaultController.php
+++ src/WMS/BackendBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('WMSBackendBundle:Default:index.html.twig');
+        return $this->redirect('/backend');
     }
 }
